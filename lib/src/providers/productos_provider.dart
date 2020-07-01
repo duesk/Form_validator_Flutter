224fe424358263518f82c71df3462c06b95f2ead
Colibri <colibri@MacBook-Pro-de-Ricardo.local>
2020-06-30 19:23:41 -0500

    upload image is done

--- lib/src/providers/productos_provider.dart
+++ lib/src/providers/productos_provider.dart
@@ -1,8 +1,12 @@
 
 import 'dart:convert';
+//import 'dart:html';
 import 'dart:io';
 
+import 'package:mime_type/mime_type.dart';
+
 import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 import 'package:form_validation/src/model/producto_model.dart';
 
 class ProductosProvider{
@@ -66,7 +70,32 @@ class ProductosProvider{
   Future<String> subirImage( File imagen ) async {
 
     final url = Uri.parse("https://api.cloudinary.com/v1_1/dql22gfad/image/upload?upload_preset=ml_default");
+    final mimeType = mime(imagen.path).split("/");
+
+    final imageUploadRequest = http.MultipartRequest(
+      "post",
+      url
+    );
+
+    final file = await http.MultipartFile.fromPath(
+      "file",
+      imagen.path,
+      contentType: MediaType( mimeType[0], mimeType[1])
+    );
     
+    imageUploadRequest.files.add(file);
+    // imageUploadRequest.files.add(file); // SI necesitas subir mas archivos.
+
+
+    final streamResponse = await imageUploadRequest.send();
+    final resp = await http.Response.fromStream(streamResponse);
+
+    if (resp.statusCode != 200 && resp.statusCode !=201){
+      print("error algo no funciona: ");
+      print(resp.body);
+      return null;
+    }
+
   }
 
 
